4ab310b04320ca473cd791e6f3aaf3b30cc35886
isolationforjob <isolationforjob@gmail.com>
2021-09-13 15:16:31 +0300

    first commit

---
+++ HabrTitleParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HabrTitleParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
